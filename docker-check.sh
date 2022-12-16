cbf6601cb11a2889bb5137652bc316a9870a4c02
Jj1910 <32131060+Jj1910@users.noreply.github.com>
2022-12-16 17:19:19 -0500

    Add files via upload

---
+++ docker-check.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+docker ps -a
+echo ""
+docker images -a
+echo ""
+docker volume ls
+echo ""
+docker system df
---
+++ docker-cleanup.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+yes | docker image prune -a
+yes | docker system prune -a
+docker volume rm $(docker volume ls)
