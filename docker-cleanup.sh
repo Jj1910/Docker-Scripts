4705dde0bda90c931886cbffa485d726355518df
Jj1910 <32131060+Jj1910@users.noreply.github.com>
2023-01-24 15:41:53 -0500

    Update docker-cleanup.sh

--- docker-cleanup.sh
+++ docker-cleanup.sh
@@ -3,4 +3,4 @@ docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 yes | docker image prune -a
 yes | docker system prune -a
-docker volume rm $(docker volume ls -q)
+docker volume rm $(docker volume ls -q | grep -wv nameOfVolumetoSkip)
