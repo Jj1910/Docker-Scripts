bd53cd33c6ef8738fd0d12ee0098862345ac886d
Jj1910 <32131060+Jj1910@users.noreply.github.com>
2023-01-25 08:33:59 -0500

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -4,3 +4,4 @@ docker rm $(docker ps -a -q)
 yes | docker image prune -a
 yes | docker system prune -a
 docker volume rm $(docker volume ls -q)  #Add the following after -q to skip volumes that should be saved | grep -wv nameOfVolumetoSkip)
+docker network rm $(docker network ls -q) #Only use if not cleaning up images by commenting out lines 4 and 5
