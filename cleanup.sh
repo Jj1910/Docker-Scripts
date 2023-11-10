524d8364d3340178a3eee369a138e6342e388533
Jj1910 <32131060+Jj1910@users.noreply.github.com>
2023-11-10 11:32:16 -0500

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -4,4 +4,4 @@ docker rm $(docker ps -a -q)
 yes | docker image prune -a
 yes | docker system prune -a
 docker network rm $(docker network ls -q)
-docker volume rm $(docker volume ls -q | grep -wv rss-data | grep -wv traefik-ssl-certs | grep -wv wordpress-data | grep -wv wordpress-db)
+docker volume rm $(docker volume ls -q | grep -wv rss-data | grep -wv traefik-ssl-certs | grep -wv wordpress-data | grep -wv wordpress-db | grep -wv portainer-data | grep -wv heimdall-data)
